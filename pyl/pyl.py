0eb4ad801fbf79b6dc82e3c254cf3b0c1404c834
Larry Xue <larry.p.xue@gmail.com>
2020-09-01 13:34:43 -0400

    commit

--- pyl/pyl.py
+++ pyl/pyl.py
@@ -4,31 +4,34 @@ import sys
 import contextlib
 import importlib
 
-def run(code, files=None, filter_=None, header=None, imports=None):
+def run(code, files=None, filter_=None, begin=None, end=None, imports=None):
 	if files is None:
 		files = [sys.stdin]
 	if imports is not None:
 		for imp in imports:
 			importlib.import_module(imp)
-	if header is not None:
-		exec(header)
+	if begin is not None:
+		exec(begin)
 	if filter_ is not None:
 		filter_ = compile(filter_, "<string>", "eval")
-	code = compile(code, "<string>", "single")
+	code = compile(code, "<string>", "eval")
 	for file in files:
 		for i, line in enumerate(file):
 			line = line.rstrip("\n")
 			x = l = line
-			if filter_ is not None and not exec(filter_):
+			if filter_ is not None and not eval(filter_):
 				continue
-			exec(code)
+			print(eval(code))
+	if end is not None:
+		exec(end)
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-H", "--header", help="code to run at start of each line")
-	parser.add_argument("-f", "-F", "--filter", help="code to filter lines to run")
+	parser = argparse.ArgumentParser(prog="pyl")
+	parser.add_argument("-b", "-H", "--begin", help="code to run at the beginning")
+	parser.add_argument("-e", "--end", help="code to run at the end")
+	parser.add_argument("-f", "--filter", help="code to filter lines to run")
 	parser.add_argument(
-		"-i", "-I", "--import", dest="imports", help="modules to import before running code"
+		"-i", "--import", dest="imports", help="modules to import before running code"
 	)
 	parser.add_argument("code", nargs="?", help="code to run per line")
 	parser.add_argument("files", nargs="*", default=["-"], help="list of files")
@@ -37,13 +40,17 @@ def main():
 		imports = []
 	else:
 		imports = [imp.strip() for imp in args.imports.split(",")]
-	if args.filter is not None and args.code is None:
-		args.code = "l"
+	if args.code is None:
+		if args.filter is not None:
+			args.code = "l"
+		else:
+			parser.error("At least one of code or filter must be provided")
+	
 	with contextlib.ExitStack() as stack:
 		files = [
 			stack.enter_context(open(file)) if file != "-" else sys.stdin for file in args.files
 		]
-		run(args.code, files, args.filter, args.header, imports)
+		run(args.code, files, args.filter, args.begin, args.end, imports)
 
 if __name__ == "__main__":
 	main()
